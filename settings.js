24d21304b52f53fd0ddf010c56eca351014ed320
krishansandaruwan <125231210+krishansandaruwan@users.noreply.github.com>
2023-02-13 08:10:48 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94719225807'] //ur owner number
-global.ownername = "©Qᴜᴇᴇɴ-x ᴡᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀ ʙᴏᴛ" //ur owner name
+global.owner = ['94715368981'] //ur owner number
+global.ownername = "©krishan sandaruwan" //ur owner name
 global.ytname = "ʏᴜʀᴇꜱʜ" //ur yt chanel name
-global.socialm = "👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻" //ur github or insta name
+global.socialm = "👸🏻krishan🏻" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.ownernomer = "94719225807" //ur number
+global.ownernomer = "94715368981" //ur number
 global.premium = ['94719225807'] //ur premium number
-global.botname = '©Qᴜᴇᴇɴ-x ᴡᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀ ʙᴏᴛ' //ur bot name
-global.linkz = "https://chat.whatsapp.com/Fd5MPEBn7NQ1aviL4V5fa7" //your theme url which will be displayed on whatsapp
-global.websitex = "https:// no" //ur website to be displayed
-global.botscript = 'https://github.com/yureshkav/Queen-X' //script link
+global.botname = '©krishan sandaruwan' //ur bot name
+global.linkz = "https://youtu.be/y9X1jTT9FbI" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtu.be/y9X1jTT9FbI" //ur website to be displayed
+global.botscript = 'https://youtu.be/y9X1jTT9FbI' //script link
 global.themeemoji = "🛸" //ur theme emoji
-global.packname = "   👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻" //ur sticker watermark packname
-global.author = "   👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻\n\n\nA whatsapp bot developed by\nCYBORG ROBOT" //ur sticker watermark author
-global.wm = "👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻." //ur watermark
+global.packname = "   👸🏻©krishan sandaruwan👸🏻" //ur sticker watermark packname
+global.author = "   👸🏻©krishan sandaruwan👸🏻\n\n\nA whatsapp bot developed by\nCYBORG ROBOT" //ur sticker watermark author
+global.wm = "👸🏻©krishan sandaruwan👸🏻." //ur watermark
 
 // Other
 global.sessionName = 'session'
